b0b54d0763e9fa8a7906b34e942e802fb49eb406
Inkkonu <killian.blain@outlook.com>
2022-12-02 09:46:51 +0100

    Day 2

--- Day2.py
+++ Day2.py
@@ -1,9 +1,11 @@
 def p1(data):
-    return None
+    dic = {'AY': 8, 'AX': 4, 'AZ': 3, 'BY': 5, 'BX': 1, 'BZ': 9, 'CY': 2, 'CX': 7, 'CZ': 6}
+    return sum([dic[line[0] + line[2]] for line in data])
 
 
 def p2(data):
-    return None
+    dic = {'AY': 4, 'AX': 3, 'AZ': 8, 'BY': 5, 'BX': 1, 'BZ': 9, 'CY': 6, 'CX': 2, 'CZ': 7}
+    return sum([dic[line[0] + line[2]] for line in data])
 
 
 if __name__ == '__main__':
