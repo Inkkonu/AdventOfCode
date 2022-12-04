721d09ee56ef8bba1e4c58399538214990903ba0
Inkkonu <killian.blain@outlook.com>
2022-12-04 15:48:59 +0100

    Day 4 was too simple

--- Day4.py
+++ Day4.py
@@ -1,9 +1,21 @@
 def p1(data):
-    return None
+    t = 0
+    for line in data:
+        e1, e2 = line.split(',')[0], line.split(',')[1]
+        s1, s2 = {i for i in range(int(e1.split('-')[0]), int(e1.split('-')[1])+1)}, {i for i in range(int(e2.split('-')[0]), int(e2.split('-')[1])+1)}
+        if s1.issubset(s2) or s2.issubset(s1):
+            t += 1
+    return t
 
 
 def p2(data):
-    return None
+    t = 0
+    for line in data:
+        e1, e2 = line.split(',')[0], line.split(',')[1]
+        s1, s2 = {i for i in range(int(e1.split('-')[0]), int(e1.split('-')[1]) + 1)}, {i for i in range(int(e2.split('-')[0]),int(e2.split('-')[1]) + 1)}
+        if len(s1.intersection(s2)) != 0:
+            t += 1
+    return t
 
 
 if __name__ == '__main__':
