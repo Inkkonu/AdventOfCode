cccad79bd4f196a13d6a095803cfcf7d3ebeb124
Inkkonu <killian.blain@outlook.com>
2022-12-07 23:12:06 +0100

    Fuck you Python, I love you Java

---
+++ Day9.java
@@ -0,0 +1,26 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Day9 {
+
+    public static int p1(List<String> data){
+        return 0;
+    }
+
+    public static int p2(List<String> data){
+        return 0;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        List<String> data = new ArrayList<>();
+        Scanner scanner = new Scanner(new File("input.txt"));
+        while (scanner.hasNextLine()) {
+            data.add(scanner.nextLine());
+        }
+        System.out.println("Partie 1 : " + p1(data));
+        System.out.println("Partie 2 : " + p2(data));
+    }
+}
