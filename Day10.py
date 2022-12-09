e31281089fe727bcdb396f9481dfa08cdaabe33d
Inkkonu <killian.blain@outlook.com>
2022-12-09 21:46:23 +0100

    don't look at the codeé

---
+++ Day10.py
@@ -0,0 +1,12 @@
+def p1(data):
+    return None
+
+def p2(data):
+    return None
+
+
+if __name__ == '__main__':
+    with open('input.txt', 'r') as f:
+        data = [line.strip() for line in f.readlines()]
+    print(f'Part 1 : {p1(data)}')
+    print(f'Part 2 : {p2(data)}')
