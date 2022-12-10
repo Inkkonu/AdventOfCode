8e6514e84d29a40e9d0b89dad484ecfbd8db56bc
Inkkonu <killian.blain@outlook.com>
2022-12-10 14:21:59 +0100

    easy day 10

--- Day10.py
+++ Day10.py
@@ -1,8 +1,45 @@
 def p1(data):
-    return None
+    d = dict()
+    cycle = x = 1
+    for line in data:
+        if line == 'noop':
+            cycle += 1
+            if cycle in {20, 60, 100, 140, 180, 220}:
+                d[cycle] = x
+        else:
+            v = int(line.split(' ')[1])
+            cycle += 1
+            if cycle in {20, 60, 100, 140, 180, 220}:
+                d[cycle] = x
+            cycle += 1
+            x += v
+            if cycle in {20, 60, 100, 140, 180, 220}:
+                d[cycle] = x
+    return sum([k * d[k] for k in d])
+
 
 def p2(data):
-    return None
+    crt = [['.' for _ in range(40)] for _ in range(6)]
+    cycle = x = 1
+    for line in data:
+        if line == 'noop':
+            if x - 1 <= (cycle - 1) % 40 <= x + 1:
+                crt[cycle // 40][(cycle - 1) % 40] = '#'
+            cycle += 1
+        else:
+            v = int(line.split(' ')[1])
+            if x - 1 <= (cycle - 1) % 40 <= x + 1:
+                crt[cycle // 40][(cycle - 1) % 40] = '#'
+            cycle += 1
+            if x - 1 <= (cycle - 1) % 40 <= x + 1:
+                crt[cycle // 40][(cycle - 1) % 40] = '#'
+            cycle += 1
+            x += v
+
+    s = '\n'
+    for line in crt:
+        s += ' '.join(map(str, line)) + "\n"
+    return s
 
 
 if __name__ == '__main__':
