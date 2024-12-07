0e6752c383034db8657404797458d6028d01edb6
Inkkonu <61760122+Inkkonu@users.noreply.github.com>
2024-12-07 14:18:38 +0100

    product from itertools is OP

---
+++ aoc2024/Day07.py
@@ -0,0 +1,46 @@
+from itertools import product
+
+
+def p1(data):
+    total = 0
+    for line in data:
+        line = line.split(":")
+        goal, numbers = int(line[0]), list(map(int, line[1].split(" ")[1:]))
+        combinations = product(["+", "*"], repeat=len(numbers)-1)
+        for combination in combinations:
+            if evaluate_expression(numbers, combination) == goal:
+                total += goal
+                break
+    return total
+
+
+def evaluate_expression(numbers, operators):
+    total = numbers[0]
+    for i, op in enumerate(operators):
+        if op == "+":
+            total += numbers[i+1]
+        elif op == "*":
+            total *= numbers[i+1]
+        elif op == "||":
+            total = int(str(total) + str(numbers[i+1]))
+    return total
+
+
+def p2(data):
+    total = 0
+    for line in data:
+        line = line.split(":")
+        goal, numbers = int(line[0]), list(map(int, line[1].split(" ")[1:]))
+        combinations = product(["+", "*", "||"], repeat=len(numbers)-1)
+        for combination in combinations:
+            if evaluate_expression(numbers, combination) == goal:
+                total += goal
+                break
+    return total
+
+
+if __name__ == '__main__':
+    with open('input.txt', 'r') as f:
+        data = [line.strip() for line in f.readlines()]
+    print(f'Part 1 : {p1(data)}')
+    print(f'Part 2 : {p2(data)}')
