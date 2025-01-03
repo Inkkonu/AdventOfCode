392d1a4af69d84f18c65779c597917192efac3b7
Inkkonu <61760122+Inkkonu@users.noreply.github.com>
2025-01-03 21:52:06 +0100

    added times for 2021

--- aoc2021/Day01.py
+++ aoc2021/Day01.py
@@ -1,13 +1,25 @@
+import time
+
+
 def p1(data):
     return sum([1 if data[i] > data[i - 1] else 0 for i in range(len(data))])
 
 
 def p2(data):
-    return sum([1 if sum(data[i:i + 3]) > sum(data[i - 1:i - 1 + 3]) else 0 for i in range(1, len(data))])
+    return sum(
+        [
+            1 if sum(data[i : i + 3]) > sum(data[i - 1 : i - 1 + 3]) else 0
+            for i in range(1, len(data))
+        ]
+    )
 
 
-if __name__ == '__main__':
-    with open('input.txt', 'r') as f:
+if __name__ == "__main__":
+    with open("input.txt", "r") as f:
         data = [int(line.strip()) for line in f.readlines()]
-    print(f'Part 1 : {p1(data)}')
-    print(f'Part 2 : {p2(data)}')
+    start = time.time()
+    print(f"Part 1 : {p1(data)}")
+    print(f"Time for part 1 : {time.time() - start}s")  # 269.41 μs
+    start = time.time()
+    print(f"Part 2 : {p2(data)}")
+    print(f"Time for part 2 : {time.time() - start}s")  # 658.51 μs
--- aoc2021/Day02.py
+++ aoc2021/Day02.py
@@ -1,13 +1,16 @@
+import time
+
+
 def p1(data):
     x = depth = 0
     for line in data:
-        parts = line.split(' ')
+        parts = line.split(" ")
         instruc, n = parts[0], int(parts[1])
-        if instruc == 'forward':
+        if instruc == "forward":
             x += n
-        elif instruc == 'down':
+        elif instruc == "down":
             depth += n
-        elif instruc == 'up':
+        elif instruc == "up":
             depth -= n
     return x * depth
 
@@ -15,20 +18,24 @@ def p1(data):
 def p2(data):
     x = depth = aim = 0
     for line in data:
-        parts = line.split(' ')
+        parts = line.split(" ")
         instruc, n = parts[0], int(parts[1])
-        if instruc == 'forward':
+        if instruc == "forward":
             x += n
             depth += aim * n
-        elif instruc == 'down':
+        elif instruc == "down":
             aim += n
-        elif instruc == 'up':
+        elif instruc == "up":
             aim -= n
     return x * depth
 
 
-if __name__ == '__main__':
-    with open('input.txt', 'r') as f:
+if __name__ == "__main__":
+    with open("input.txt", "r") as f:
         data = [line.strip() for line in f.readlines()]
-    print(f'Part 1 : {p1(data)}')
-    print(f'Part 2 : {p2(data)}')
+    start = time.time()
+    print(f"Part 1 : {p1(data)}")
+    print(f"Time for part 1 : {time.time() - start}s")  # 335.93 μs
+    start = time.time()
+    print(f"Part 2 : {p2(data)}")
+    print(f"Time for part 2 : {time.time() - start}s")  # 245.09 μs
